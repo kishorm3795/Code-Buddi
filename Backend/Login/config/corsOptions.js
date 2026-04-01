5f30204ed46e0fe6d48fd09cab36f71c3edc63ca
PAVAN KISHOR M <kishorm3795@gmail.com>
2026-04-01 21:43:10 +0530

    removing all the files

--- Backend/Login/config/corsOptions.js
+++
@@ -1,8 +0,0 @@
-const corsOptions = {
-	origin: '*',
-	methods: ['GET', 'POST', 'PUT', 'DELETE'],
-	allowedHeaders: ['Content-Type', 'Authorization', 'x-recaptcha-token'],
-	credentials: true,
-};
-
-module.exports = corsOptions;
--- Backend/Login/utils/otpGenerator.js
+++
@@ -1,7 +0,0 @@
-const crypto = require('node:crypto');
-
-function generateOtp() {
-  return crypto.randomBytes(3).toString('hex');
-}
-
-module.exports = { generateOtp };
